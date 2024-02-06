7fcc644f62005a40c547fbcb4c583cd6af9f6115
Kamazop <95763578+Kamazop@users.noreply.github.com>
2024-02-06 21:53:51 +0200

    Update Main.java
    Kolmanda ül. mingi algus

--- zoppKodu1/src/Main.java
+++ zoppKodu1/src/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("1. " + symbolidKeskel("abba"));
         System.out.println("2. " + sisaldabAlams6ne("abaxaaaxc"));
+        System.out.println("3. " + kordusteArv(new int[]{3, 2, 1, 4, 55, 2, 1, 9, 0, 2}, 2));
     }
 
     // 1. Olgu meetodi sisendiks suvalise pikkusega sõne (pikkus võib olla ka 0). Kui sõne
@@ -13,6 +14,7 @@ public class Main {
     // pikkus on paarituarvuline, tahame vastuseks saada sõne 3 keskmist sümbolit. Kui
     // sõne on liiga lühike, tuleb tagastada kogu sõne (ehk siis juhtudel, kui sõne pikkus on 0
     // või 1).
+    
     public static String symbolidKeskel(String s6ne) {
         int pikkus = s6ne.length();
         if (pikkus <= 1) { // Kui sõne on liiga lühike
@@ -35,5 +37,18 @@ public class Main {
             return false;
         }
         return s6ne.substring(3).contains("aaa");
+    }
+    // 3. Olgu meetodi sisendiks täisarvude massiiv ja üks täisarv. Vastuseks tahame teada,
+    // kui mitu korda etteantud arv selles massiivis esineb.
+    
+    public static int kordusteArv(int[] massiiv, int arv) {
+        int kordusteArv = 0;
+        for (int i = 0;) {
+            for (int i = 0; 1 < massiiv.length; i++) {
+            if (massiiv[i] == 0) {
+                
+            }
+        }
+        return kordusteArv;
     }
 }
