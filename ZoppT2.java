3349473c84675c6bb46824fcbfad21c9c7668ce4
Kamazop <95763578+Kamazop@users.noreply.github.com>
2024-02-20 09:51:46 +0200

    Create ZoppT2.java

---
+++ ZoppT2.java
@@ -0,0 +1,8 @@
+// Kõik ülesanded teha meetoditena, mmille main-meetod etteantud argumentidega välja kutsub. Pidage kinni meetodite spetsifikatsioonist. Vastused väljastatakse konsoolile.
+
+// 1. Olgu antud täisarvude massiiv. Anda vastuseks teise ja viimase liikme korrutis. Kui massiivis on ainult 1 element, siis on vastuseks see element ja kui massiivi pikkus on 0, on vastuseks 0.
+static int korrutis(int[]jada)
+  {2,3,4,6}  18
+  {8}  8
+  {}  0
+// 2. On antud 2 sõnet.
