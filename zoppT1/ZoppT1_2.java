27e2ffc483ed0efa199eef47662efa8dbdc68929
Kamazop <95763578+Kamazop@users.noreply.github.com>
2024-05-26 15:10:25 +0300

    Add files via upload

---
+++ zoppT1/ZoppT1_2.java
@@ -0,0 +1,24 @@
+package zoppT1;
+
+public class ZoppT1_2 {
+    public static void main(String[] args) {
+        int a = 13;
+        int b = 30;
+
+        if (a % 3 == 0) { //Kontrollin kas esimene arv jagub 3-ga
+            System.out.println("True");
+        } else if (b % 3 == 0) {//teen sama teise arvuga
+            System.out.println("True");
+        } else { //ehk siis kui ei jagu kumbi 3-ga siis on tulemuseks väär
+            System.out.println("False");
+        }
+    }
+}
+/*
+Tunniülesanne 1
+
+2. On antud kaks täisarvu (muutujat). Väljastada tõeväärtus “true”, kui vähemalt üks
+jagub kolmega, ja muidu “false”.
+Nt. 13 ja 32 ==> vastuseks “false”
+ 25 ja 135 ==> vastuseks “true”
+ */
